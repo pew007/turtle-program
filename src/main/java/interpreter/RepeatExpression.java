b43fab8f87d6ffc4b39dec295454dcab723687ac
Pengyu Wang <bird0407@gmail.com>
2018-10-11 16:34:05 -0700

    implement repeat expression

--- src/main/java/interpreter/RepeatExpression.java
+++ src/main/java/interpreter/RepeatExpression.java
@@ -1,10 +1,22 @@
 package interpreter;
 
+import java.util.List;
+
 public class RepeatExpression extends Expression {
 
-    private Expression[] expressions;
+    private int repetition;
+    private List<Expression> expressions;
+
+    public RepeatExpression(int repetition, List<Expression> expressions) {
+        this.repetition = repetition;
+        this.expressions = expressions;
+    }
 
     public void evaluate(Context context) {
-
+        for (int i = 0; i < this.repetition; i++) {
+            for (Expression expression : this.expressions) {
+                expression.evaluate(context);
+            }
+        }
     }
 }
